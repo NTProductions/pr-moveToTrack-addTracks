81dff9e47979b908199154a234be84c04c59320f
Nate Lovell <Natelovell99@gmail.com>
2021-06-05 17:25:57 -0600

    Create moveToTrack and addTracks.jsx

---
+++ moveToTrack and addTracks.jsx
@@ -0,0 +1,14 @@
+﻿app.enableQE();
+
+var vanillaSequence = app.project.activeSequence;
+var qeSequence = qe.project.getActiveSequence(0);
+
+qeSequence.addTracks(10, vanillaSequence.videoTracks.numTracks, 10, vanillaSequence.audioTracks.numTracks);
+    // argument 1, num video tracks to add
+    // argument 2, track to add them after
+    // argument 3, num audio tracks to add
+    // argument 4, track to add them after
+    
+var thisQEClip = qeSequence.getVideoTrackAt(0).getItemAt(0);
+    
+thisQEClip.moveToTrack(500, 0, "0");
