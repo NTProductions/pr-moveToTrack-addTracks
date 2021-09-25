883bc1935f6940af07e4ba34c7aa4dc6424b3f9f
Nate Lovell <Natelovell99@gmail.com>
2021-09-25 12:35:01 -0600

    updated argument information

--- moveToTrack and addTracks.jsx
+++ moveToTrack and addTracks.jsx
@@ -5,9 +5,21 @@ var qeSequence = qe.project.getActiveSequence(0);
 
 qeSequence.addTracks(10, vanillaSequence.videoTracks.numTracks, 10, vanillaSequence.audioTracks.numTracks);
     // argument 1, num video tracks to add
-    // argument 2, track to add them after
+    // argument 2, track to add them after (zero-based index)
     // argument 3, num audio tracks to add
-    // argument 4, track to add them after
+    // argument 4, track to add them after (zero-based index)
+    // number of submixes to add
+    // submix type (see below)
+    // position to add submix (zero-based index)
+
+
+    // submix type guide
+    // 0: Mono
+    // 1: Standard
+    // 2: 5.1
+    // 3: Stereo
+
+    // thanks to @calebvetter for the updated information
     
 var thisQEClip = qeSequence.getVideoTrackAt(0).getItemAt(0);
     
